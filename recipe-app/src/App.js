2fe8990feb4f8a83391b2c1c723f54cd283e6f79
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-08-08 15:41:18 +0300

    Update App.js

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -21,8 +21,11 @@ function App() {
   const getRecipes = async () => {
     const response = await fetch(
       `https://api.edamam.com/${query}?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+      //{ mode: "no-cors" }
     );
+
     const data = await response.json();
+
     console.log(data.hits);
     setRecipes(data.hits);
     console.log(data.hits);
