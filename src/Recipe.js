993f953a7b04a39804b870bad434e87472692631
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-08-23 00:44:59 +0300

    small changes

--- src/Recipe.js
+++ src/Recipe.js
@@ -6,12 +6,14 @@ function Recipe({ title, calories, image, ingredients, data }) {
     <div className={style.recipe}>
       <h1 className={style.title}>{title}</h1>
       <p>Calories {calories.toFixed(0)}</p>
-      <ul>
-        {ingredients.map((ingredient) => (
-          <li>{ingredient.text}</li>
-        ))}
-      </ul>
-      <img className={style.image} src={image} alt="Food" />
+      <div className={style.ingredients_photo}>
+        <ul>
+          {ingredients.map((ingredient) => (
+            <li>{ingredient.text}</li>
+          ))}
+        </ul>
+        <img className={style.image} src={image} alt="Food" />
+      </div>
     </div>
   );
 }
